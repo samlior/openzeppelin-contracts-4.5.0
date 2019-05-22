2e94b287c7cead7a6c0603205670566461c31abb
Francisco Giordano <frangio.1@gmail.com>
2019-05-22 11:56:25 -0300

    fix docsite-start script

--- scripts/docsite.sh
+++ scripts/docsite.sh
@@ -4,11 +4,12 @@
 
 set -o errexit
 
+npm run docgen
+
 if [ "$1" = start ]; then
   npx concurrently -n docgen,docsite --no-color \
-    'nodemon -w contracts -e sol,md -x npm run docgen' \
+    'nodemon -C -w contracts -e sol,md -x npm run docgen' \
     'openzeppelin-docsite-run start'
 else
-  npm run docgen
   npx openzeppelin-docsite-run "$1"
 fi
