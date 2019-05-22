0fa4160484309d0851584fe57c0d81a3600977db
Francisco Giordano <frangio.1@gmail.com>
2019-05-22 11:47:44 -0300

    improve docsite start script (automatically watch docgen)

--- scripts/docsite.sh
+++ scripts/docsite.sh
@@ -4,5 +4,11 @@
 
 set -o errexit
 
-npm run docgen
-npx openzeppelin-docsite-run "$1"
+if [ "$1" = start ]; then
+  npx concurrently -n docgen,docsite --no-color \
+    'nodemon -w contracts -e sol,md -x npm run docgen' \
+    'openzeppelin-docsite-run start'
+else
+  npm run docgen
+  npx openzeppelin-docsite-run "$1"
+fi
