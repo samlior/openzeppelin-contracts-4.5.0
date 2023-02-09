adc2d63edcd823de08b47007a4828c9952364209
samlior <samlior@foxmail.com>
2023-02-09 15:27:35 +0800

    chore: use 0.8.4, add etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,53 +6,53 @@
 // - COMPILE_VERSION:   compiler version (default: 0.8.9)
 // - COINMARKETCAP:     coinmarkercat api key for USD value in gas report
 
-const fs = require('fs');
-const path = require('path');
-const argv = require('yargs/yargs')()
-  .env('')
+const fs = require("fs");
+const path = require("path");
+const argv = require("yargs/yargs")()
+  .env("")
   .options({
     ci: {
-      type: 'boolean',
+      type: "boolean",
       default: false,
     },
     coverage: {
-      type: 'boolean',
+      type: "boolean",
       default: false,
     },
     gas: {
-      alias: 'enableGasReport',
-      type: 'boolean',
+      alias: "enableGasReport",
+      type: "boolean",
       default: false,
     },
     mode: {
-      alias: 'compileMode',
-      type: 'string',
-      choices: [ 'production', 'development' ],
-      default: 'development',
+      alias: "compileMode",
+      type: "string",
+      choices: ["production", "development"],
+      default: "development",
     },
     compiler: {
-      alias: 'compileVersion',
-      type: 'string',
-      default: '0.8.9',
+      alias: "compileVersion",
+      type: "string",
+      default: "0.8.4",
     },
     coinmarketcap: {
-      alias: 'coinmarketcapApiKey',
-      type: 'string',
+      alias: "coinmarketcapApiKey",
+      type: "string",
     },
-  })
-  .argv;
+  }).argv;
 
-require('@nomiclabs/hardhat-truffle5');
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-etherscan");
 
 if (argv.enableGasReport) {
-  require('hardhat-gas-reporter');
+  require("hardhat-gas-reporter");
 }
 
-for (const f of fs.readdirSync(path.join(__dirname, 'hardhat'))) {
-  require(path.join(__dirname, 'hardhat', f));
+for (const f of fs.readdirSync(path.join(__dirname, "hardhat"))) {
+  require(path.join(__dirname, "hardhat", f));
 }
 
-const withOptimizations = argv.enableGasReport || argv.compileMode === 'production';
+const withOptimizations = argv.enableGasReport || argv.compileMode === "production";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -74,13 +74,42 @@ module.exports = {
     },
   },
   gasReporter: {
-    currency: 'USD',
-    outputFile: argv.ci ? 'gas-report.txt' : undefined,
+    currency: "USD",
+    outputFile: argv.ci ? "gas-report.txt" : undefined,
     coinmarketcap: argv.coinmarketcap,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "avax",
+        chainId: 43114,
+        urls: {
+          apiURL: "https://api.snowtrace.io/api",
+          browserURL: "https://snowtrace.io",
+        },
+      },
+      {
+        network: "bsc",
+        chainId: 56,
+        urls: {
+          apiURL: "https://api.bscscan.com/api",
+          browserURL: "https://bscscan.com/",
+        },
+      },
+      {
+        network: "arb",
+        chainId: 42161,
+        urls: {
+          apiURL: "https://api.arbiscan.io/api",
+          browserURL: "https://arbiscan.io/",
+        },
+      },
+    ],
   },
 };
 
 if (argv.coverage) {
-  require('solidity-coverage');
+  require("solidity-coverage");
   module.exports.networks.hardhat.initialBaseFeePerGas = 0;
 }
